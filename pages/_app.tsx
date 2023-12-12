57ee6b70863c6419209fe7975f222eda825e38b9
AnkurSharmaCrownstack <ankur.sharma@crownstack.com>
2023-12-12 17:35:26 +0530

    update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import Layout from '../components/Layout';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return
-    <>
-        <Component {...pageProps} />
-        <SpeedInsights />
-    </>;
+    return (
+        <>
+            <Component {...pageProps} />
+            <SpeedInsights />
+        </>
+    );
 }
 
 export default MyApp;
